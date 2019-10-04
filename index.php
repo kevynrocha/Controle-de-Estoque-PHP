cd150678a3ce804b49aa426bd2ab8d29dd0fe1b5
Kevyn Rocha <kevyn_oliver@hotmail.com>
2019-10-04 18:25:47 -0400

    adicionado index

--- index.php
+++ index.php
@@ -1,25 +1,17 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style-index.css">
-    <title>Controle de Estoque</title>
-</head>
-<body>    
-    <div class="login">
-        <h1>Login</h1>
-        <form action="" method="post">
-            <input type="text" name="u" placeholder="Usuario" required="required" />
-            <input type="password" name="p" placeholder="Senha" required="required" />
-            <button type="submit" class="btn btn-primary btn-block btn-large">Entrar.</button>
-        </form>
-    </div>
+<?php 
 
+require_once('lib/vendor/autoload.php');
 
-    
+use Models\ClassCrud;
 
+$crud = new ClassCrud;
+$crud->selectDB(
+    "*",
+    "produtos",
+    "",
+    array()
+);
 
-    
-</body>
-</html>
+$f = $bf->fetch(PDO::FETCH_ASSOC);
+
+var_dump($f);
